1c2cea9ded62532f434a809dacbbdd51ebb726f4
Mdigo12 <mdigojacob@gmail.com>
2022-09-14 14:05:39 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""1-square.py
+This a simple class to define Square
+with attribute size
+"""
+
+
+class Square:
+    """
+    This is the class with attribute size
+    """
+
+    pass
+
+    def __init__(self, size):
+        '''
+        Class initialization function
+        Args:
+            size (integer): a size attribute passed
+        '''
+        self.__size = size
