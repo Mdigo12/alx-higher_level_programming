6aec78597c5258c4d90a2992e00d771cce981972
Mdigo12 <mdigojacob@gmail.com>
2022-09-14 16:18:57 -0700

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+'''
+a class Square that defines a square
+'''
+
+
+class Square:
+    """
+    class with private attribute size and validates it
+
+    raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialization
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        finding the area of the square
+
+        Returns:
+            int: the are of the square
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """
+        Printing the size of the square
+        """
+        if self.__size == 0:
+            print()
+        for r in range(self.__size):
+            print("#"*self.__size)
