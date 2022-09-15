3348adfe7802c56762a05a8d11137bed5a57a319
Mdigo12 <mdigojacob@gmail.com>
2022-09-14 20:33:31 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,8 +69,10 @@ class Square:
         """
         if self.__size == 0:
             print()
-        for x in range(self.__position[1]):
-            print
+        """for x in range(self.__position[1]):
+             print
+        """
+        print(''*self.__position[1], end="")
         for r in range(self.__size):
             print(" "*self.position[0], end="")
             print("#"*self.__size)
