b5342ec0bb7548f335d7f564f3def5e143e130ff
Mdigo12 <mdigojacob@gmail.com>
2022-09-27 10:47:24 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,11 +5,11 @@
 
 def read_file(filename=""):
     """
-    Reads a file object
+    Reads a file object and prints data to stdout
 
     Args:
         filename (str): name of the file
     """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
