04e6f2750568563ef8f648e8148389877fac2481
Mdigo12 <mdigojacob@gmail.com>
2022-09-27 10:31:17 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""""0-read_file.py"""
+
+
+def read_file(filename=""):
+    """
+    Reads a file object
+
+    Args:
+        filename (str): name of the file
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
