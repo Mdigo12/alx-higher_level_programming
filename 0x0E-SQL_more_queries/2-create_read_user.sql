83127a092d322dc209af67011ddd597bc191fc36
Mdigo12 <mdigojacob@gmail.com>
2022-11-03 14:19:52 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,3 +5,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- query privilegs for user_0d_2
 -- GRANT USAGE ON * . * TO 'user_0d_2'@'localhost';
 GRANT SELECT ON `hbtn_0d_2` . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
