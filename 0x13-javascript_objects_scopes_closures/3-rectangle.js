88f519d53afeb0d3ff4e0483064facd541e4c96c
Mdigo12 <mdigojacob@gmail.com>
2022-11-29 10:07:05 -0800

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,15 +2,15 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if ((w <= 0) || (h <= 0) || !h || !w) {
-      return this;
+      return this
     }
-    this.width = w;
-    this.height = h;
+    this.width = w
+    this.height = h
   }
-  
+
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log('X'.repeat(this.width))
     }
   }
-};
+}
