3de9ed566e53811dba9236f78ea007e7853a0cfe
Mdigo12 <mdigojacob@gmail.com>
2022-12-16 03:40:14 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
 
+from urllib import request
+
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
         response = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(response)))
-        print("    - content: {}".format(response))
-        print("    - utf8 content: {}" .format(
-            response.decode(encoding='utf-8')))
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
