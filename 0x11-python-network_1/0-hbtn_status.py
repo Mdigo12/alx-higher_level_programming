3c6e8c7c097aec8ca93ace695a96b509493579e4
Mdigo12 <mdigojacob@gmail.com>
2022-12-16 02:50:38 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-import urllib.request
 
 if __name__ == "__main__":
+    import urllib.request
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         response = response.read()
         print("Body response:")
