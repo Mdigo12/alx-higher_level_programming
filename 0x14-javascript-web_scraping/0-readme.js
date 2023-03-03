55544d20273f87a8eac7ab76765ee6982c81c268
Mdigo12 <mdigojacob@gmail.com>
2023-03-03 09:34:07 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,12 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-if (process.argv.length > 2){
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err){
-            console.log(err);
-        }
-        else {
-            console.log(data);
-        }
-    });
+if (process.argv.length > 2) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
 }
