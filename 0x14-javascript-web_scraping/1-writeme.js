8eb1c393ac48350c4bc07250b7336e114a3004ac
Mdigo12 <mdigojacob@gmail.com>
2023-03-03 09:32:45 +0300

    check requirement

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const contentText = process.argv[3];
 
-fs.writeFile(filePath, contentText, 'utf-8', (err) => {
-    if (err){
-        console.log(err);
-    }
-    // else {
-    //     console.log('The file ' + filePath +' has been saved with the following content:\n'+ contentText);
-    // }
+fs.writeFile(filePath, contentText, 'utf-8', err => {
+  if (err) {
+    console.log(err);
+  }
+  // else {
+  //     console.log('The file ' + filePath +' has been saved with the following content:\n'+ contentText);
+  // }
 });
