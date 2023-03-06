317afcaf6761d8c90ab04c42b72aafb34d65a024
Mdigo12 <mdigojacob@gmail.com>
2023-03-06 06:57:32 +0300

    fix semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 // Send a GET request to the movie endpoint
 request.get(movieUrl, (err, response, body) => {
@@ -12,8 +12,7 @@ request.get(movieUrl, (err, response, body) => {
 
   // Parse the response body as JSON
   const movie = JSON.parse(body);
-  
+
   // If it matches, print the movie title
   console.log(movie.title);
-
 });
