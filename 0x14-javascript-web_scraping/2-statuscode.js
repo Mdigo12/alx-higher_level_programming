8b2ad172c16d8ffd42370d8d77a830b093f28f27
Mdigo12 <mdigojacob@gmail.com>
2023-03-06 05:41:35 +0300

    fixed backticks

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,5 +7,5 @@ request.get(url, (error, response) => {
   if (error) {
     console.log(error);
   }
-  console.log('code: ${response.statusCode}');
+  console.log(`code: ${response.statusCode}`);
 });
