1fdb12d74f2963cd23d5047c983482f7e9b2f988
Mdigo12 <mdigojacob@gmail.com>
2023-04-26 05:05:39 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,16 +6,13 @@ The content of the file must be read in utf-8
 If an error occurred during the reading, print the error object
 */
 
-const { error } = require('console');
 const fs = require('fs');
+const filePath = process.argv[2]; // get file path from command line
 
-const file_path = process.argv[2]; // get file path from command line
-
-fs.readFile(file_path, 'utf-8', (error,data) => {
-    if(error){
-        console.log(error);
-    } else {
-        console.log(data);
-    }
-
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
