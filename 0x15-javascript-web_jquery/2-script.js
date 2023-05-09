9433ba72fb256df07afa5df68be43494540ce87f
Mdigo12 <mdigojacob@gmail.com>
2023-05-09 21:57:15 +0300

    2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,10 @@
+// A JavaScript script that updates the text color of the <header> element to red\
+// when the user clicks on the tag DIV#red_header:
+//      You can’t use document.querySelector to select the HTML tag
+//      You must use the JQuery API
+
+$('div#red_header').click(
+  function () {
+    $('header').css('color', '#FF0000');
+  }
+);
