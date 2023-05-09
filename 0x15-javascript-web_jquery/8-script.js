fcc87edcb3d8133ec1af205b596035e3a705c8c1
Mdigo12 <mdigojacob@gmail.com>
2023-05-09 23:11:22 +0300

    populate li with data from api

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,14 @@
+/**
+ * a JavaScript script that fetches and lists the title for all movies
+ * by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
+ *
+ * All movie titles must be list in the HTML tag UL#list_movies
+ * You can’t use document.querySelector to select the HTML tag
+ * You must use the JQuery API
+ */
+
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', (data) => {
+  $.each(data.results, (i, movie) => {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
