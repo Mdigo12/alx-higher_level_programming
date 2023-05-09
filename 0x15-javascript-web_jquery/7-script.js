8a6f7b3359baaf27174994254ba2b957dd312469
Mdigo12 <mdigojacob@gmail.com>
2023-05-09 22:56:44 +0300

    use .get to fetch data from api url

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+/*
+a JavaScript script that fetches the character name
+from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+
+The name must be displayed in the HTML tag DIV#character
+You can’t use document.querySelector to select the HTML tag
+You must use the JQuery API
+*/
+
+$.getJSON('https://swapi-api.alx-tools.com/api/people/5/?format=json', (data) => {
+  $('DIV#character').text(data.name);
+});
