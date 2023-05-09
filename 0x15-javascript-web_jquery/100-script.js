32ba68715337ce619820313a09d4442567050671
Mdigo12 <mdigojacob@gmail.com>
2023-05-10 00:34:36 +0300

    querySelector to update text color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,12 @@
+/**
+ * Write a JavaScript script that updates the text color
+ * of the <header> element to red (#FF0000):
+ *
+ * You can’t use the jQuery API
+ * Note: Your script must be imported from the <head> tag, not at the end of the HTML
+ */
+
+window.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
