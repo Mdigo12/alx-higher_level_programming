98a33b41357150f639a6c31457bf111f0c958d13
Mdigo12 <mdigojacob@gmail.com>
2023-05-09 21:56:54 +0300

    1-script.js

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// A JavaScript script that updates the text color of the <header> element to red
+// You can’t use document.querySelector to select the HTML tag
+// You must use the JQuery API
+
+$('header').css('color', '#FF0000');
